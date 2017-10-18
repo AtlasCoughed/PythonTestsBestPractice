d4007290368d0cb1d23269b9551012051fec95df
Michael <michaelhuy@google.com>
2017-10-18 15:28:32 -0700

    .

--- PythonBestPractices.py
+++ PythonBestPractices.py
@@ -9,7 +9,7 @@ def testGetProfitAfterRevSharing(self):
      #TODO(someone): this test is failing even though I know its correct - better fix code!
      self.assertEqual(45, result.getCents())
 
-# Bad practice
+#  START - Bad practice
 
 def testGetProfitAfterRevSharing(self):
     profit = money.Cents(90)
@@ -18,3 +18,36 @@ def testGetProfitAfterRevSharing(self):
     rev_share3 = profit_calculator.RevShare(percent=10)
 
     result = profit_calculator.GetProfitAfterRevSharing(profit, rev_share1, rev_share2, rev_share3)
+
+    self.assertEqual(
+        profit.GetMicros() - profit.GetMicros()
+        * (rev_share1.GetFraction() + rev_share2.GetFraction() + rev_share2.GetFraction()),
+        result.getMicros()
+    )
+# END - Bad Practice
+
+
+# GOOD - CHOOSE Good representative values
+
+def testMultiply_TwoPositiveValues_ProducesPositiveValue(self):
+    self.assertEqual(6, my_math.Multiply(2,3 ))
+
+def testMultiply_TwoNegativeAndPositiveValues_ProducesNegativeValue(self):
+    self.assertEqual(6, my_math.Multiply(-2,3 ))
+
+def testMultiply_PositiveAndNegativeValues_ProducesNegativeValue(self):
+        self.assertEqual(6, my_math.Multiply(2, -3))
+
+def testMultiply_TwoNegativeValues_ProducesPositiveValue(self):
+    self.assertEqual(6, my_math.Multiply(-2, -3 ))
+
+# END - GOOD
+
+# START - BAD - Covers the same case of the same behavior multiple times:
+
+def testMultiply(self):
+  self.assertEqual(6, my_math.Multiply(2, 3))
+  self.assertEqual(8, my_math.Multiply(2, 4))
+  self.assertEqual(15, my_math.Multiply(3, 5))
+
+# END - BAD
